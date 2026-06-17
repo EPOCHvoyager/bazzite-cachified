124e5e5059cb58d6354c75c19a913655a8d090d6
Crono <39300993+EPOCHvoyager@users.noreply.github.com>
2026-06-17 12:07:00 -0300

    Install compiler for akmods build

--- build_files/build.sh
+++ build_files/build.sh
@@ -41,7 +41,8 @@ rpmkeys --import /etc/pki/rpm-gpg/RPM-GPG-KEY-terra"${RELEASE}"
 dnf5 install -y \
     --enablerepo="terra" \
     --setopt=tsflags=noscripts \
-    akmod-zenergy-*.fc"${RELEASE}"."${ARCH}"
+    akmod-zenergy-*.fc"${RELEASE}"."${ARCH}" \
+    gcc-c++
 mkdir -p /var/tmp
 chmod 1777 /var/tmp
 akmods --force --kernels "${KERNEL_VERSION}" --kmod zenergy
